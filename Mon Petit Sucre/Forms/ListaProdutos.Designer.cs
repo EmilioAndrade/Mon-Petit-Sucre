290f1c68b3d3a9970eb5aab5cdee83e2c5b84171
Emilio Moreira de Andrade <Duilio_Andrade@hotmail.com>
2017-10-17 22:50:16 -0200

    Criação Form Lista Produtos.

---
+++ Mon Petit Sucre/Forms/ListaProdutos.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Mon_Petit_Sucre.Forms
+{
+    partial class ListaProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListaProdutos));
+            this.listViewProdutos = new System.Windows.Forms.ListView();
+            this.columnProduto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDescricao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnPreco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.lblAlterar = new System.Windows.Forms.Label();
+            this.lblDeletar = new System.Windows.Forms.Label();
+            this.lblVoltar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listViewProdutos
+            // 
+            this.listViewProdutos.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listViewProdutos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnProduto,
+            this.columnDescricao,
+            this.columnPreco,
+            this.columnStatus});
+            this.listViewProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewProdutos.FullRowSelect = true;
+            this.listViewProdutos.GridLines = true;
+            this.listViewProdutos.Location = new System.Drawing.Point(12, 12);
+            this.listViewProdutos.Name = "listViewProdutos";
+            this.listViewProdutos.Size = new System.Drawing.Size(869, 402);
+            this.listViewProdutos.TabIndex = 0;
+            this.listViewProdutos.UseCompatibleStateImageBehavior = false;
+            this.listViewProdutos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnProduto
+            // 
+            this.columnProduto.Text = "Produto";
+            this.columnProduto.Width = 200;
+            // 
+            // columnDescricao
+            // 
+            this.columnDescricao.Text = "Descrição";
+            this.columnDescricao.Width = 300;
+            // 
+            // columnStatus
+            // 
+            this.columnStatus.DisplayIndex = 2;
+            this.columnStatus.Text = "Status";
+            this.columnStatus.Width = 100;
+            // 
+            // columnPreco
+            // 
+            this.columnPreco.DisplayIndex = 3;
+            this.columnPreco.Text = "Preço";
+            this.columnPreco.Width = 120;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.Location = new System.Drawing.Point(77, 442);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 44);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Image = ((System.Drawing.Image)(resources.GetObject("btnAlterar.Image")));
+            this.btnAlterar.Location = new System.Drawing.Point(310, 442);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 44);
+            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Image = ((System.Drawing.Image)(resources.GetObject("btnDeletar.Image")));
+            this.btnDeletar.Location = new System.Drawing.Point(558, 442);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 44);
+            this.btnDeletar.TabIndex = 3;
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
+            this.btnVoltar.Location = new System.Drawing.Point(749, 442);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 44);
+            this.btnVoltar.TabIndex = 4;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Location = new System.Drawing.Point(49, 498);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(135, 20);
+            this.lblAdd.TabIndex = 5;
+            this.lblAdd.Text = "Adicionar Produto";
+            // 
+            // lblAlterar
+            // 
+            this.lblAlterar.AutoSize = true;
+            this.lblAlterar.Location = new System.Drawing.Point(283, 498);
+            this.lblAlterar.Name = "lblAlterar";
+            this.lblAlterar.Size = new System.Drawing.Size(131, 20);
+            this.lblAlterar.TabIndex = 6;
+            this.lblAlterar.Text = "Atualizar Produto";
+            // 
+            // lblDeletar
+            // 
+            this.lblDeletar.AutoSize = true;
+            this.lblDeletar.Location = new System.Drawing.Point(539, 498);
+            this.lblDeletar.Name = "lblDeletar";
+            this.lblDeletar.Size = new System.Drawing.Size(115, 20);
+            this.lblDeletar.TabIndex = 7;
+            this.lblDeletar.Text = "Excluir Produto";
+            // 
+            // lblVoltar
+            // 
+            this.lblVoltar.AutoSize = true;
+            this.lblVoltar.Location = new System.Drawing.Point(762, 498);
+            this.lblVoltar.Name = "lblVoltar";
+            this.lblVoltar.Size = new System.Drawing.Size(51, 20);
+            this.lblVoltar.TabIndex = 8;
+            this.lblVoltar.Text = "Voltar";
+            // 
+            // ListaProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 532);
+            this.Controls.Add(this.lblVoltar);
+            this.Controls.Add(this.lblDeletar);
+            this.Controls.Add(this.lblAlterar);
+            this.Controls.Add(this.lblAdd);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listViewProdutos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ListaProdutos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista de Produtos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewProdutos;
+        private System.Windows.Forms.ColumnHeader columnProduto;
+        private System.Windows.Forms.ColumnHeader columnDescricao;
+        private System.Windows.Forms.ColumnHeader columnPreco;
+        private System.Windows.Forms.ColumnHeader columnStatus;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.Label lblAlterar;
+        private System.Windows.Forms.Label lblDeletar;
+        private System.Windows.Forms.Label lblVoltar;
+    }
+}
